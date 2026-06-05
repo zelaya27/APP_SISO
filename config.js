b6df395c8098c0aa9f8a481c97fdf23aaefcdd4a
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-05 10:30:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config_nuevo.js
 const CONFIG = {
-    URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbxO7Ztj1mG8wGyOTPwYIsA5T7z31LNX4Ix6-E_uhSbhzQtLU0uCHxlpYMtS0OVMZVMw/exec"
+    URL_APPS_SCRIPT: "https://script.google.com/macros/s/AKfycbyCsGV1lvo4Ij2CN3vEtOFzHqXzIKvuhh1YdTHMSDwLD-SRaz0B3UEpYBxoW44YlDjX/exec"
 };
